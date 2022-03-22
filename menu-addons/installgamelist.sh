c762fec4515f58a1bc448cdbe207f927e601627b
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2022-03-22 15:35:47 +0100

    cambiamos la ruta del gamelist de retropie a /home/pi

--- menu-addons/installgamelist.sh
+++ menu-addons/installgamelist.sh
@@ -13,7 +13,7 @@ echo "Conexion con GitHub.com establecida, continuando..."
 sleep 2
 echo "Descargando y copiando el gamelist original de la imagen y reinstalando el menu Addons..."
 sleep 2
-wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoinspi4/main/menu-addons/patchs/gamelist.xml>/opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoinspi4/main/menu-addons/patchs/gamelist.xml>/home/pi/RetroPie/retropiemenu/gamelist.xml
 wget -O- https://raw.githubusercontent.com/julenvitoria/6goldencoinspi4/main/menu-addons/ActualizarMenu.sh | bash
 
 echo "Fix de instalacion con gamelist original aplicado!!!!"
